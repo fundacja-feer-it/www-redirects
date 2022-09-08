2900d7629a615536d858ff02946d9b846bca4c13
Ziemowit Gil <ziemowit.gil@feer.org.pl>
2022-09-08 18:08:35 +0200

    Create repo

---
+++ scripts/create.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ];then
+    echo "Nie masz uprawnień do wykonania skryptu"
+    exit 1
+fi
+whiptail --title "URL Redirects engine " --msgbox "Skrypt tworzy nowe przekierowanie" 10 60
+DOMENA=$(whiptail --title "Zródło" --inputbox "Jaka jest nazwa domeny?" 10 60  3>&1 1>&2 2>&3)
+URL=$(whiptail --title "Cel" --inputbox "URL docelowy: " 10 60  3>&1 1>&2 2>&3)
+sudo mkdir -p /var/www/$DOMENA/public_html
+sudo chown -R $USER:$USER /var/www/$DOMENA/public_html
+sudo chmod -R 755 /var/www
+nano /var/www/$DOMENA/public_html/index.html
+sudo systemctl restart apache2
